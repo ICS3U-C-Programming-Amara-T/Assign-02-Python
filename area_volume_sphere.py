ce67f4ed24648ac9f109ba8eca768f2c88ee35b4
Amara Tie <amara.tie@stu.ocsb.ca>
2025-03-08 04:06:23 +0000

    area_volume_sphere.py

--- area_volume_sphere.py
+++ area_volume_sphere.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-
 # Created By: Amara Tie
-
 # Date: Month March 4, 2025
-
 # Calculating the surface area and volume of a sphere.
 import math
 import colorama
@@ -13,21 +10,24 @@ colorama.init(autoreset=True)
 
 
 def main():
+    #Greeting
+    print("Hello! Lets find the surface area and volume of a sphere!")
+
     # Get Radius
     radius = float(input("Enter the radius of the sphere : "))
     units = input("Enter Units: ")
 
-    # Calculate the surface area and volume
+     # Calculate the surface area and volume
     surface_area = 4 * math.pi * radius**2
     volume = 4 / 3 * math.pi * radius**3
 
-    # Color your text Source:https://www.youtube.com/watch?v=u51Zjlnui4Y
+    # Color your text. Source:https://www.youtube.com/watch?v=u51Zjlnui4Y
     print("\nChoose a color")
     print("1. magenta")
     print("2. blue")
-    print("3. yellow")
+    print("3. green")
     print("4. red")
-    color_choice = input("Enter the # (1-5)")
+    color_choice = input("Enter the # (1-4)")
 
     # Color choices to Fore Colors
     if color_choice == "1":
@@ -35,15 +35,14 @@ def main():
     if color_choice == "2":
         color = Fore.BLUE
     if color_choice == "3":
-        color = Fore.YELLOW
+        color = Fore.GREEN
     if color_choice == "4":
         color = Fore.RED
 
     # Display the volume, surface area and color
     print("")
-    print(f"Color: {color}")
-    print(f"Volume: {color} {volume:.2f}^3".format(volume, units))
-    print(f"Surface Area: {color}{surface_area:.2f}^2".format(surface_area, units))
+    print(f"Volume: {color} {volume:.2f} {units}^3".format(volume, units))
+    print(f"Surface Area: {color}{surface_area:.2f} {units}^2".format(surface_area, units))
 
 
 if __name__ == "__main__":
